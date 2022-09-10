449eadf3aa11d89c7cd2db7f60ecc8ebcd5c0e7a
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-09-10 09:45:41 +0700

    Add routing to /katalog, add data in katalog.html

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
-# TODO: Create your views here.
+from katalog.models import CatalogItem
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+    'name': 'Marcellinus Elbert',
+    'student_id': '2106752400',
+    'list_katalog': data_catalog_item
+    }
+    return render(request, 'katalog.html',context)
