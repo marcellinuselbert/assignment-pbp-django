2a187f343a27ac4f310164ecadf14e99a5856d8a
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-09-11 19:36:53 +0700

    Implement unit test for views

--- katalog/tests/test_views.py
+++ katalog/tests/test_views.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 class TestViews(TestCase):
 
     def test_show_catalog_resolved(self):
-        pass
+  
         
         # this code give strange error??
         
-        # client = Client()
-        # response = client.get(reverse("katalog:katalog"))
-        # self.assertEquals(response.status_code,200)
-        # self.assertTemplateUsed(response,'katalog.html')
+        client = Client()
+        response = client.get(reverse("katalog:katalog"))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'katalog.html')
         
