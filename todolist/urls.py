57ff5965e9a5dd48ef1d2978b84ef3af24d9f493
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-10-08 19:15:38 +0700

    Assignment 6 done

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todolist.views import delete_todo, show_todolist, register,login_user,logout_user, show_create_todo, update_finished, delete_todo
+from todolist.views import add_todo, delete_todo,show_json, show_todolist, register,login_user,logout_user, show_create_todo, update_finished, delete_todo
 
 app_name = 'todolist'
 
@@ -9,6 +9,8 @@ urlpatterns = [
     path('login/', login_user, name='login'), 
     path('logout/', logout_user, name='logout'),
     path('create-task/', show_create_todo, name='show_create_todo'),
+    path('add/', add_todo, name='add_todo'),
+    path('json/', show_json, name='show_json'),
     path('<id>/update/', update_finished, name='update'),
     path('<id>/delete/', delete_todo, name='delete'),
 
--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,6 @@
+from dataclasses import fields
 import datetime
+from urllib import response
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core import serializers
 from django.urls import reverse
@@ -8,6 +10,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
 from todolist.models import TodoListEntry
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
 @login_required(login_url='login/')
@@ -78,3 +82,28 @@ def delete_todo(request,id):
     task.delete()
 
     return redirect('todolist:show_todolist')
+
+def show_json(request):
+    data = TodoListEntry.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@csrf_exempt
+def add_todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        todo = TodoListEntry.objects.create(title=title, description=description,date=datetime.date.today(), is_finished=False, user=request.user)
+
+        result = {
+            'fields':{
+                'title':todo.title,
+                'description':todo.description,
+                'is_finished':todo.is_finished,
+                'date':todo.date,
+            },
+            'pk':todo.pk
+        }
+
+
+
+        return JsonResponse(result)
